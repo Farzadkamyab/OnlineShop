98c3632811514d0b643f246d566a804646c0a311
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 16:20:45 +0330

    add logout url

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("signup/", UserSignUp.as_view(), name="signUp"),
     path("login/", UserLogin.as_view(), name="login"),
     path("otp-check/", CheckOtp.as_view(), name="otp"),
+    path("logout/", UserLogout.as_view(), name="logout"),
 ]
