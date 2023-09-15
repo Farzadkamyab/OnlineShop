9f7e22f8230c71bc3c199993676248598cbde65c
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 15:46:07 +0330

    add user login serializer

--- users/serializer.py
+++ users/serializer.py
@@ -1,10 +1,4 @@
-from rest_framework import serializers
-from .models import CustomUser, Address
-
-class UserSignUpSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField()
-
+class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["phone_number", "password", "password2", "first_name", "last_name"]
-
+        fields = ["phone_number", "password"]
