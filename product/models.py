4352d6acb710991b70c2f8230a35c05952f62d82
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 15:31:50 +0330

    add product model

--- product/models.py
+++ product/models.py
@@ -5,3 +5,11 @@ from orders.models import OrderItem
 class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField()
+    quantity = models.PositiveIntegerField()
+    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
