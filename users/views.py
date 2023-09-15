a17c33bbdab00ab6e5831b784829970532cf286d
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 16:21:25 +0330

    add login api view

--- users/views.py
+++ users/views.py
@@ -26,3 +26,23 @@ class UserSignUp(APIView):
         else:
             return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class UserLogin(APIView):
+    def post(self, request):
+        srz_data = UserLoginSerializer(request.POST)
+        if srz_data.is_valid():
+            random_code = random.randint(1000, 9999)
+            print(random_code)
+            CustomUser.objects.update(code=random_code)
+            phone_number = srz_data.validated_data.get("phone_number")
+            formatted_phone_number = re.sub(r"^\+98|^0098", "0", phone_number)
+
+            user = CustomUser.objects.filter(phone_number=formatted_phone_number).first()
+
+            if user is None:
+                return Response({"message": "User not found."}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                # send_OTP(formatted_phone_number, random_code)
+                request.session["user_info"] = {"phone_number": formatted_phone_number, "code": random_code}
+
+                return Response({"message": "Otp code has send successfully..."}, status=status.HTTP_200_OK)
+
