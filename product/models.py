237060b51e30fbc58e146e1ecc22ec1a28fabb00
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 16:37:43 +0330

    fix: fix decimal fields in product  models

--- product/models.py
+++ product/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
     order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
