df5ebbc6f5f94d799db2218eaa77f09ea5c47482
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 15:14:32 +0330

    add custom abstract base user model

--- users/models.py
+++ users/models.py
@@ -23,5 +23,29 @@ class PhoneNumberField(models.CharField):
         return formatted_phone_number
 
 
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone_number = PhoneNumberField(
+        _("phone number"),
+        max_length=14,
+        unique=True,
+        validators=[phone_number_validator],
+    )
+    first_name = models.CharField(_("first name"), max_length=20)
+    last_name = models.CharField(_("last name"), max_length=30)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    code = models.PositiveSmallIntegerField(null=True)
 
-# Create your models here.
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.phone_number
+
+    class Meta:
+        permissions = [
+            ("can_access_dashboard", "Can access the dashboard"),
+        ]
