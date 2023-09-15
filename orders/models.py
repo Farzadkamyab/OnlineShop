c29e978e781d1aa25c91165d08ba43851a2004de
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-15 15:31:19 +0330

    add order model

--- orders/models.py
+++ orders/models.py
@@ -4,3 +4,9 @@ from users.models import CustomUser
 class OrderItem(models.Model):
     quantity = models.PositiveIntegerField()
     item_price = models.DecimalField()
+
+class Order(models.Model):
+    item_date = models.DateTimeField()
+    total = models.DecimalField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
