9bc811069ea05cf3819c02492ade6aad8fb1acde
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-09-21 10:03:38 +0330

    add search view url

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("products/<int:pk>", ProductView.as_view(), name="products"),
+    path("search-result", SearchProductView.as_view(), name="search"),
 ]
